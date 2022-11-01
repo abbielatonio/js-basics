90c7240b3914abbb1b8d08e0da29cca1cddea034
abbie_ <abbielatonio@gmail.com>
2022-11-01 22:28:03 +0800

    initial commit

---
+++ operators.js
@@ -0,0 +1,19 @@
+//task 3.1
+let firstVal = 25;
+let secondVal = 5;
+console.log('first: ' + firstVal);
+console.log('second: ' + secondVal);
+
+//task 3.2
+console.log('firstVal + 5 = ' + (firstVal + 5));
+
+//task 3.3
+secondVal = secondVal + 5;
+console.log('secondVal + 5 = ' + secondVal);
+
+//task 3.4
+
+console.log('difference: ' + (firstVal-secondVal));
+console.log('product: ' + firstVal*secondVal);
+console.log('quotient: ' + firstVal/secondVal);
+console.log('remainder: ' + firstVal%secondVal);
---
+++ variable.js
@@ -0,0 +1,7 @@
+let country = 'Philippines';
+let populations = '112,604,142';
+let myCity = 'Muntinlupa';
+
+console.log('Country: ' + country);
+console.log('Population: ' + populations);
+console.log('My City: ' + myCity);
